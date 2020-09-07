63e5204f91c44edbffe53ced83f1c312b11fe667
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-07 11:26:06 -0300

    Praticando com os exemplos antes dos exercicios

---
+++ bloco_5/dia_1/pratica.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("paragrafo")[0].style.border = "solid";
+document.getElementsByClassName("paragrafo")[0].style.backgroundColor = "black";
+for(let i in document.getElementsByClassName("paragrafo")){
+    document.getElementsByClassName("paragrafo")[i].style.color = "red";
+}
