613ee1e9e4331a7b15b9bb83ba9d26e6196a37b3
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-12 20:01:08 -0300

    Removendo a pasta projeto

--- bloco_5/projeto/script.js
+++
@@ -1,6 +0,0 @@
-let memeInsert = document.getElementById('meme-insert');
-
-memeInsert.addEventListener('input',function(){
-    localStorage.setItem('img',memeInsert);
-    document.getElementById('meme-image').style.backgroundImage = localStorage.getItem('img');
-})
