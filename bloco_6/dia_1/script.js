05b9446ed83d6a002362f761dece230cd9111962
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-17 14:03:17 -0300

    Realizado o segundo exercio Crie outro <fieldset> para dados do seu último emprego Resumo do currículo - TextArea Limite de 1000 caracteres Campo obrigatório Cargo - Texto Limite de 40 caracteres Campo obrigatório Descrição do cargo - Texto Limite de 500 caracteres Campo obrigatório Data de início - Texto Verificar o formato da data dd/mm/aaaa. O dia deve ser > 0 e <= 31. O mês deve ser > 0 e <= 12. O ano não pode ser negativo. Caso alguma das condições for inválida no momento do envio do formulário, exibir mensagem de erro contextualizada. Campo obrigatório

--- bloco_6/dia_1/script.js
+++ bloco_6/dia_1/script.js
@@ -7,5 +7,31 @@ function gerarListaDeEstados(){
         document.getElementById('estado').appendChild(option);
     }
 }
+    function validaDia(id){
+        const data = document.getElementById(id).value.split('/',1).pop();
+        if(data < 0 && data <= 31){
+            return alert('Dia de inicio invalido');
+        }
+    }
 
+    function validaMes(id){
+        const data = document.getElementById(id).value.split('/',2).pop();
+        if(data < 1 && data <= 12){
+            return alert('Mes de inicio invalido');
+        }
+    }
+
+    function validaAno(id){
+        const data = document.getElementById(id).value.split('/',3).pop();
+        if(data < 0){
+            return alert('Ano de inicio invalido');
+        }
+    }
+
+    function validaData(id){
+        const data = document.getElementById(id).value.length;
+        if(data !== 10){
+            return alert('Digite a data no formato: dd/mm/aaaa (01/01/2020)')
+        }
+    }
 window.onload = gerarListaDeEstados();
