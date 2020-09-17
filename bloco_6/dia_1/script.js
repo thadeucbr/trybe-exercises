c20a804c4ab7d517b51955fdc8f5ab4f096b475e
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-17 20:52:48 -0300

    Passando o prettier para melhorar o codigo visualmente

--- bloco_6/dia_1/script.js
+++ bloco_6/dia_1/script.js
@@ -1,37 +1,36 @@
-function gerarListaDeEstados(){
-    const estados = ['São Paulo','Belo Horizonte','Rio de Janeiro'];
-    for(let index = 0; index < estados.length; index += 1){
-        let option = document.createElement('option');
-        option.value = estados[index];
-        option.innerText = estados[index];
-        document.getElementById('estado').appendChild(option);
-    }
+function gerarListaDeEstados() {
+	const estados = ['São Paulo', 'Belo Horizonte', 'Rio de Janeiro'];
+	for (let index = 0; index < estados.length; index += 1) {
+		let option = document.createElement('option');
+		option.value = estados[index];
+		option.innerText = estados[index];
+		document.getElementById('estado').appendChild(option);
+	}
 }
-    function validaDia(id){
-        const data = document.getElementById(id).value.split('/',1).pop();
-        if(data < 0 && data <= 31){
-            return alert('Dia de inicio invalido');
-        }
-    }
+function validaDia(id) {
+	const data = document.getElementById(id).value.split('/', 1).pop();
+	if (data < 0 || data > 31) {
+		return document.getElementById(id).setCustomValidity('Dia de inicio invalido');
+	}
+}
 
-    function validaMes(id){
-        const data = document.getElementById(id).value.split('/',2).pop();
-        if(data < 1 && data <= 12){
-            return alert('Mes de inicio invalido');
-        }
-    }
+function validaMes(id) {
+	const data = document.getElementById(id).value.split('/', 2).pop();
+	if (data > 1 || data < 12) {
+		return document.getElementById(id).setCustomValidity('Mes de inicio invalido');
+	}
+}
 
-    function validaAno(id){
-        const data = document.getElementById(id).value.split('/',3).pop();
-        if(data < 0){
-            return alert('Ano de inicio invalido');
-        }
-    }
+function validaAno(id) {
+	const data = document.getElementById(id).value.split('/', 3).pop();
+	if (data < 0) {
+		return document.getElementById(id).setCustomValidity('Ano de inicio invalido');
+	}
+}
 
-    function validaData(id){
-        const data = document.getElementById(id).value.length;
-        if(data !== 10){
-            return alert('Digite a data no formato: dd/mm/aaaa (01/01/2020)')
-        }
-    }
+document.getElementById('enviar').addEventListener('click', () => {
+	validaDia('data-inicio');
+	validaMes('data-inicio');
+	validaAno('data-inicio');
+});
 window.onload = gerarListaDeEstados();
