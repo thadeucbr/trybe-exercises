c0831c54b577dbe95b76de3275bda7c4dd03981f
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-21 16:17:10 -0300

    Validando os options

--- bloco_6/dia_2/script.js
+++ bloco_6/dia_2/script.js
@@ -20,30 +20,32 @@ new window.JustValidate('.teste', {
 			minLength: 50,
 			maxLength: 1000,
 		},
+		estado: {
+			required: true,
+			minLength: 3
+		},
 	},
 });
 
-document
-	.getElementById('data-inicio')
-	.DatePickerX.init({
-		format: 'dd/mm/yyyy',
-		mondayFirst: false,
-		weekDayLabels: ['Seg', 'Terç', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
-		singleMonthLabels: [
-			'Janeiro',
-			'Fevereiro',
-			'Março',
-			'Abril',
-			'Maio',
-			'Junho',
-			'Julho',
-			'Agosto',
-			'Setembro',
-			'Outubro',
-			'Novembro',
-			'Dezembro',
-		],
-		todayButtonLabel: 'Hoje',
-		clearButtonLabel: 'Limpar'
-	});
+document.getElementById('data-inicio').DatePickerX.init({
+	format: 'dd/mm/yyyy',
+	mondayFirst: false,
+	weekDayLabels: ['Seg', 'Terç', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
+	singleMonthLabels: [
+		'Janeiro',
+		'Fevereiro',
+		'Março',
+		'Abril',
+		'Maio',
+		'Junho',
+		'Julho',
+		'Agosto',
+		'Setembro',
+		'Outubro',
+		'Novembro',
+		'Dezembro',
+	],
+	todayButtonLabel: 'Hoje',
+	clearButtonLabel: 'Limpar',
+});
 window.onload = gerarListaDeEstados();
