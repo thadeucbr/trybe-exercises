efd94f867fe321ae45712e0e3e484032e1fd494d
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-25 16:30:33 -0300

    Removendo espaços

--- bloco_7/dia_1/exerciciosP2.js
+++ bloco_7/dia_1/exerciciosP2.js
@@ -1,14 +1,28 @@
-function findLongestWord(str){
+/* function findLongestWord(str){
     let longestWord = str.split(' ').reduce(function(longest,currentWord){
         return currentWord.length > longest.length ? currentWord : longest;
     },'');
     return console.log(longestWord);
-};
+}; */
 
-findLongestWord('Essa maracutaia maluca');
+/* findLongestWord('Essa maracutaia maluca');
 
 let clickCounter = 0;
 document.getElementById('button').addEventListener('click', () => {
     clickCounter += 1;
     document.getElementById('clicks').innerText = `Clicks: ${clickCounter}`;
-})
+}) */
+
+replaceWord = replace => {
+	let str = 'Tryber x aqui!'.replace('x', replace);
+	return str;
+};
+
+const skills = ['Ubuntu', 'HTML5', 'CSS3', 'JavaScript', 'GitHub'];
+
+bigMixer = name => {
+	skills.sort();
+	console.log(`${replaceWord(name)} ${skills}`);
+};
+
+bigMixer('Thadeu');
