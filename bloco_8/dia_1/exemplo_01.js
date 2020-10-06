a89aa339561bcda8de668bac0d19add5425a3775
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-10-06 14:32:05 -0300

    Resolvidos exercicios do dia

--- bloco_8/dia_1/exemplo_01.js
+++ bloco_8/dia_1/exemplo_01.js
@@ -1,3 +1,10 @@
-const numeros = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10];
-
-console.log(numeros.sort((a,b)=>{a-b}));
+var items = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Magnetic' },
+    { name: 'Zeros', value: 37 }
+  ];
+  items.sort((a, b) => b.value - a.value);
+  console.log(items.reverse())
