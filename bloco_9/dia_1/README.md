0ee09bfbe130b74f63a246fb9f09f8451f7d8dd7
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-10-21 18:15:06 -0300

    Adicionados exercicios 1, 2, 3, 4 e atualizado README.md

---
+++ bloco_9/dia_1/README.md
@@ -0,0 +1,7 @@
+1. Dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+
+2. Agora, dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+
+3. A função getPlanet abaixo imprime o planeta Marte de forma síncrona. Modifique getPlanet, de forma que Marte seja impresso assincronamente, depois de 4 segundos.
+
+4. Suponha que você precise simular uma mensagem enviada do robô Curiosity de Marte para a Terra. O Curiosity envia para a Terra a temperatura atual em Marte, gastando um tempo variável de até 5 segundos para que termine o envio. Crie a função sendMarsTemperature, que imprime a temperatura em Marte.
--- bloco_9/dia_1/teste.js
+++ bloco_9/dia_1/teste.js
@@ -1,40 +1,30 @@
-/* const assert = require('assert');
-
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
-
-const getUser = callback => {
-  const userToReturn = {
-    firstName: "Ivan",
-    lastName: "Ivanovich",
-    nationality: "Russian"
-  };
-  return callback(userToReturn);
-};
-
-assert.equal(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
-assert.equal(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
- */
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
 
 const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
-const getUser = (callback) => {
-  let teste = {}
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, onError) => {
   setTimeout(() => {
-    const user = {
-      firstName: "Ivan",
-      lastName: "Ivanovich",
-      nationality: "Russian"
-    };
-    teste = user;
-    console.log(teste);
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    }
+    else {
+      const errorMessage = "Country could not be found";
+      onError(errorMessage)
+    }
   }, delay());
-  return callback(teste);
 };
 
-setTimeout(() => {
-  console.log(getUser(userFullName)); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
-  console.log(getUser(userNationality)); // deve imprimir "Ivan is Russian" depois de um certo tempo
-}, delay());
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryCurrency, printErrorMessage); 
